7a6f548b6998aa953693190b89ce1a6e10f58c6a
Yatharth Sharma <yatharth0045@gmail.com>
2021-06-13 17:07:49 +0530

    added triangle program

---
+++ triangles.java
@@ -0,0 +1,44 @@
+public class triangles {
+    public static void main(String[] args) {
+        int N=20;
+        int X=N;
+        int Y=(N*2+1);
+        int L=(Y/2+1);
+        int R=(Y/2+1);
+        int Mid=N+1;
+        int L2=0;
+        int R2=0;
+        if(N%2==0){
+            L2=Mid-(N/2-1);
+            R2=Mid+(N/2-1);
+        }else{
+            L2=Mid-(N/2-1);
+            R2=Mid+(N/2-1);
+        }
+        for (int i=1; i<=X; i++)
+        {
+            for(int j=1; j<=Y; j++){
+                if(j>=L && j<=R){
+                    if(i>=X/2+1){
+                        if(j>=L2 && j<=R2){
+                            System.out.print(":");
+                        }else{
+                            System.out.print("#");
+                        }
+                    }else{
+                        System.out.print("#");
+                    }
+                }else{
+                    System.out.print(":");
+                }
+            }
+            L--;
+            R++;
+            if(i>=X/2+1){
+                L2++;
+                R2--;
+            }
+            System.out.println("");
+        }   
+    }
+}
